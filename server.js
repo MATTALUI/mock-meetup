b67a793aebf73086af144459381feffc62a40c0e
Matthew Hummer <biggreyson@gmail.com>
2019-06-12 20:03:01 -0600

    call morgan

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const port = process.env.PORT_NUMBER || 3000;
 
-app.use(morgan);
+app.use(morgan());
 
 app.use('*', (req, res, next)=>{
   res.send("hello world");
 });
 
-app.listen('8080', '0.0.0.0', ()=>{
-  console.log('listening on port 8080');
+app.listen(port, '0.0.0.0', ()=>{
+  console.log('listening on port: ', port);
 });
