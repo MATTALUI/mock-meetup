6ff4a257ff4891904d241955583380d65fd2c34c
Matthew Hummer <biggreyson@gmail.com>
2019-06-12 20:27:51 -0600

    add frontend

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 const app = express();
 const port = process.env.PORT_NUMBER || 3000;
 
 app.use(morgan());
 
+app.use(express.static(path.join(__dirname, 'frontend','build')));
+
 app.use('*', (req, res, next)=>{
-  res.send("hello world");
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 });
 
 app.listen(port, '0.0.0.0', ()=>{
