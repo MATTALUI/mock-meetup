c55ff0b51d10c33a3e475aa285a73ac24f5d29fb
Matthew Hummer <biggreyson@gmail.com>
2019-06-12 21:22:27 -0600

    add frontend router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Switch, Route, Router } from 'react-router';
+import MeetupsHomepage from './components/MeetupsHomepage.js';
 
-function App() {
+
+const App = ()=>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Switch>
+        <Route exact path="/" component={MeetupsHomepage}/>
+        <Route exact path="/bad" render={()=><h1>This is a bad route</h1>}/>
+      </Switch>
   );
 }
 
---
+++ frontend/src/components/MeetupsHomepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const App = ()=>{
+  return (
+    <div>
+      <h1>Welcome to mock Meetups!</h1>
+    </div>
+  );
+}
+
+export default App;
