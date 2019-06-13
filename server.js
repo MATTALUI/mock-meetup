15451ce10b4cb007b3de5ef25b8959f6e1253cf0
Matthew Hummer <biggreyson@gmail.com>
2019-06-12 21:27:00 -0600

    add body parser

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ const morgan = require('morgan');
 const path = require('path');
 const app = express();
 const port = process.env.PORT_NUMBER || 3000;
+const bodyParser = require('body-parser');
 
 app.use(morgan());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'frontend','build')));
 
